d2e980212cd0abd4746def59b8661bd037df0bff
LAPTOP-CCI3445D\31380 <3138068793@qq.com>
2021-01-05 14:03:58 +0800

    U 修改仓库获取信息错误

--- src/views/wrh-management/overflow-bill/add.vue
+++ src/views/wrh-management/overflow-bill/add.vue
@@ -551,7 +551,7 @@ export default {
         const postData = {
           page: 1,
           pageSize: 0,
-          statusEquals: 'OFF'
+          statusEquals: 'ON'
         }
         StorageService.warehouseInit(postData)
         .then((res) => {
